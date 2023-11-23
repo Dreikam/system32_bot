1a4a652f96595ee28afc106617df18ef01f78bde
Dreikam <DreikamCorrea4004@hotmail.com>
2023-11-23 18:51:05 -0300

    feat: hola mundo

--- src/Routers/Guild.router.ts
+++ src/Routers/Guild.router.ts
@@ -6,6 +6,11 @@ const app = Router();
 const controller = new GuildController();
 
 app.get('/:guildId', controller.getGuild);
+app.get('/', (req, res) => {
+  res.json({
+    data: 'Hola desde guilds',
+  });
+});
 app.post('/', controller.createGuild);
 app.patch('/:guildId', controller.updateGuild);
 app.delete('/:guildId', controller.deleteGuild);
