5f75a6c4526c643b2623e9083d11d8a7912058f6
Dreikam <DreikamCorrea4004@hotmail.com>
2023-11-23 18:54:52 -0300

    fix: rutas

--- src/Routers/Guild.router.ts
+++ src/Routers/Guild.router.ts
@@ -5,12 +5,12 @@ import { GuildController } from '@Controllers/Guilds.controller';
 const app = Router();
 const controller = new GuildController();
 
-app.get('/:guildId', controller.getGuild);
 app.get('/', (req, res) => {
   res.json({
     data: 'Hola desde guilds',
   });
 });
+app.get('/:guildId', controller.getGuild);
 app.post('/', controller.createGuild);
 app.patch('/:guildId', controller.updateGuild);
 app.delete('/:guildId', controller.deleteGuild);
