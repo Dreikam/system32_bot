c2617f3eb6c0caa5a58a593a6ac113bd7f9a4c45
Dreikam <dreikamcorrea4004@hotmail.com>
2023-12-23 17:44:38 -0300

    feat: new guild route

--- src/Routers/Guild.router.ts
+++ src/Routers/Guild.router.ts
@@ -5,6 +5,7 @@ import { GuildController } from '@Controllers/Guilds.controller';
 const app = Router();
 const controller = new GuildController();
 
+app.get('/', controller.getGuilds);
 app.get('/:guildId', controller.getGuild);
 app.post('/', controller.createGuild);
 app.patch('/:guildId', controller.updateGuild);
