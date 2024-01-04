7bbed8254bfa155ac6d4eb26b509755a58e1f435
Dreikam <DreikamCorrea4004@hotmail.com>
2024-01-04 00:39:27 -0300

    feat: tokens crud

--- src/Routers/Tokens.router.ts
+++ src/Routers/Tokens.router.ts
@@ -5,6 +5,7 @@ import { TokensController } from '@Controllers/Tokens.controller';
 const app = Router();
 const controller = new TokensController();
 
+app.get('/', controller.getAllTokens);
 app.get('/:id', controller.getToken);
 app.post('/', controller.createToken);
 app.patch('/:id', controller.updateToken);
--- src/Utils/Tokens.ts
+++ src/Utils/Tokens.ts
@@ -1,6 +1,6 @@
 export const generateToken = () => {
   let d = new Date().getTime();
-  const uuid = 'zxxx-exxx-exxx-wxxx'.replace(/[xy]/g, (c) => {
+  const uuid = 'dxxxx-rxxxx-kxxxx-mxxxx'.replace(/[xy]/g, (c) => {
     let r = (d + Math.random() * 16) % 16 | 0;
     d = Math.floor(d / 16);
     return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
